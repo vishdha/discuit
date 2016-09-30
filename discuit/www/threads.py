159310327d545caf9f03fa9ff601f6fec24a4d16
Vishal <vishaldhayagude07@gmail.com>
2016-09-30 11:14:49 +0530

    threads page category filter addded

--- discuit/www/threads.py
+++ discuit/www/threads.py
@@ -2,17 +2,19 @@
 import frappe
 
 def get_context(context):
-	thr = frappe.get_all("Thread", fields=["name", "th_thread_title"])
-	thread_post_count = []
-	for i in thr:
-		thread_post_count.append({
-			"name": i.name,
-			"post_count": frappe.db.count("Post", filters={"pst_thread":i.name}),
-			"thread_title": i.th_thread_title
-		})
-	context.threads = thread_post_count
-	context.no_cache = 1
-	return context
+    thr = frappe.get_all("Thread", fields=["name", "th_thread_title","th_categories"])
+    thread_post_count = []
+    for i in thr:
+        thread_post_count.append({
+            "name": i.name,
+            "post_count": frappe.db.count("Post", filters={"pst_thread":i.name}),
+            "thread_title": i.th_thread_title,
+            "thread_category":i.th_categories
+
+        })
+    context.threads = thread_post_count
+    context.no_cache = 1
+    return context
 
 
 @frappe.whitelist()
